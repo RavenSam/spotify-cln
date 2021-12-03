f2b1b08b33b1c5f2a6a618164f2bfad9ead93ed5
Sam Makhlouf <maksam7dev@gmail.com>
2021-12-03 15:28:10 +0100

    first commit

---
+++ components/Sidebar.jsx
@@ -0,0 +1,53 @@
+import { Fragment } from "react"
+import { signOut, useSession } from "next-auth/react"
+import {
+   HomeIcon,
+   SearchIcon,
+   LibraryIcon,
+   PlusCircleIcon,
+   HeartIcon,
+   RssIcon,
+   LogoutIcon,
+} from "@heroicons/react/outline"
+
+const BarBtns = [
+   { name: "Home", icon: HomeIcon },
+   { name: "Search", icon: SearchIcon },
+   { name: "Your Library", icon: LibraryIcon },
+   { name: "Create Playlist", icon: PlusCircleIcon },
+   { name: "Liked Songs", icon: HeartIcon },
+   { name: "Your Episodes", icon: RssIcon },
+]
+
+export default function Sidebar() {
+   const { data: session, status } = useSession()
+
+   return (
+      <div className="text-gray-500 p-5 text-sm md:text-base border-r border-gray-500">
+         <div className="space-y-4">
+            {BarBtns.map((btn, index) => (
+               <Fragment key={index}>
+                  <button className="flex items-center space-x-2 hover:text-white">
+                     <btn.icon className="h-5 w-5" />
+                     <span>{btn.name}</span>
+                  </button>
+
+                  {(index === 2 || index === 5) && <hr className="border-t-[0.1px] border-gray-900" />}
+               </Fragment>
+            ))}
+
+            <button onClick={() => signOut()} className="flex items-center space-x-2 hover:text-white">
+               <LogoutIcon className="h-5 w-5" />
+               <span>Log Out</span>
+            </button>
+
+            {/* Playlist */}
+            {[...Array(15)].map((x) => (
+               <p key={x} className="cursor-pointer hover:text-white">
+                  Playlist Name...
+               </p>
+            ))}
+         </div>
+      </div>
+   )
+}
---
+++ pages/login.js
@@ -0,0 +1,33 @@
+import { getProviders, signIn } from "next-auth/react"
+
+export default function Login({ providers }) {
+   // console.log(process.env.NEXT_PUBLIC_CLIENT_SECRET)
+
+   return (
+      <div className="flex flex-col items-center  justify-center bg-black min-h-screen w-full">
+         <img src="/spotify.png" alt="Spotify Logo" className="w-52 mb-9" />
+
+         {/* <h1 className="text-2xl font-bold mb-4">Login With</h1> */}
+         {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+               <button
+                  className="bg-[#1ed760] text-white px-7 py-2 rounded-full font-semibold "
+                  onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+               >
+                  Login With {provider.name}
+               </button>
+            </div>
+         ))}
+      </div>
+   )
+}
+
+export async function getServerSideProps() {
+   const providers = await getProviders()
+
+   return {
+      props: {
+         providers,
+      },
+   }
+}
